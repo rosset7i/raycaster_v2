e6cd6d0ba849adabac3a0b59d29df81efb1d7e1a
Matheus Rossetti <mh.rossetti2002@gmail.com>
2025-12-21 10:59:20 -0300

    added rendering and shading

--- src/main.rs
+++ src/main.rs
@@ -11,23 +11,22 @@ const MAP_WIDTH: usize = 12;
 const MAP_HEIGHT: usize = 12;
 const SCREEN_WIDTH: u32 = 640;
 const SCREEN_HEIGHT: u32 = 480;
-const MAP: [[u8; MAP_HEIGHT]; MAP_WIDTH] = [
+const MAP: [[u8; MAP_WIDTH]; MAP_HEIGHT] = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1],
+    [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 ];
 
 fn main() {
-    // Setup logic
     let event_loop = EventLoop::new().unwrap();
     let window = {
         let size = LogicalSize::new(SCREEN_WIDTH, SCREEN_HEIGHT);
@@ -49,15 +48,17 @@ fn main() {
         Pixels::new(SCREEN_WIDTH, SCREEN_HEIGHT, surface_texture).unwrap()
     };
 
-    // Raycasting logic
-    let pos_x: f32 = 11.0;
-    let pos_y: f32 = 6.0;
+    let mut pos_x: f32 = 6.0;
+    let mut pos_y: f32 = 6.0;
 
-    let dir_x: f32 = -1.0;
-    let dir_y: f32 = 0.0;
+    let mut dir_x: f32 = -1.0;
+    let mut dir_y: f32 = 0.0;
 
-    let plane_x: f32 = 0.0;
-    let plane_y: f32 = 0.66;
+    let mut plane_x: f32 = 0.0;
+    let mut plane_y: f32 = 0.66;
+
+    const MOVE_SPEED: f32 = 0.1;
+    const ROT_SPEED: f32 = 0.1;
 
     #[allow(deprecated)]
     let _ = event_loop
@@ -70,21 +71,64 @@ fn main() {
                     {
                         match key {
                             KeyCode::Escape => window_target.exit(),
+                            KeyCode::KeyW => {
+                                if MAP[(pos_x + dir_x * MOVE_SPEED) as usize][pos_y as usize] == 0 {
+                                    pos_x += dir_x * MOVE_SPEED;
+                                }
+
+                                if MAP[pos_x as usize][(pos_y + dir_y * MOVE_SPEED) as usize] == 0 {
+                                    pos_y += dir_y * MOVE_SPEED;
+                                }
+                            }
+                            KeyCode::KeyS => {
+                                if MAP[(pos_x - dir_x * MOVE_SPEED) as usize][pos_y as usize] == 0 {
+                                    pos_x -= dir_x * MOVE_SPEED;
+                                }
+
+                                if MAP[pos_x as usize][(pos_y - dir_y * MOVE_SPEED) as usize] == 0 {
+                                    pos_y -= dir_y * MOVE_SPEED;
+                                }
+                            }
+                            KeyCode::KeyA => {
+                                let cos = ROT_SPEED.cos();
+                                let sin = ROT_SPEED.sin();
+
+                                let old_dir_x = dir_x;
+                                dir_x = dir_x * cos - dir_y * sin;
+                                dir_y = old_dir_x * sin + dir_y * cos;
+
+                                let old_plane_x = plane_x;
+                                plane_x = plane_x * cos - plane_y * sin;
+                                plane_y = old_plane_x * sin + plane_y * cos;
+                            }
+                            KeyCode::KeyD => {
+                                let cos = (-ROT_SPEED).cos();
+                                let sin = (-ROT_SPEED).sin();
+
+                                let old_dir_x = dir_x;
+                                dir_x = dir_x * cos - dir_y * sin;
+                                dir_y = old_dir_x * sin + dir_y * cos;
+
+                                let old_plane_x = plane_x;
+                                plane_x = plane_x * cos - plane_y * sin;
+                                plane_y = old_plane_x * sin + plane_y * cos;
+                            }
                             _ => (),
                         }
                     }
                 }
                 WindowEvent::RedrawRequested => {
+                    pixels.frame_mut().fill(0);
                     for x_stripe in 0..SCREEN_WIDTH {
                         let camera_x = 2.0 * x_stripe as f32 / SCREEN_WIDTH as f32 - 1.0;
                         let ray_dir_x = dir_x + plane_x * camera_x;
                         let ray_dir_y = dir_y + plane_y * camera_x;
 
-                        let map_x = pos_x as u8;
-                        let map_y = pos_y as u8;
+                        let mut map_x = pos_x as i32;
+                        let mut map_y = pos_y as i32;
 
-                        let side_dist_x: f32;
-                        let side_dist_y: f32;
+                        let mut side_dist_x: f32;
+                        let mut side_dist_y: f32;
 
                         let delta_dist_x = if ray_dir_x == 0.0 {
                             1e30
@@ -98,15 +142,79 @@ fn main() {
                         };
                         let perp_wall_dist: f32;
 
-                        let step_x: u32;
-                        let step_y: u32;
+                        let step_x: i32;
+                        let step_y: i32;
 
-                        let hit: u32 = 0;
-                        let side: u32;
+                        let mut hit: u32 = 0;
+                        let mut side: u32 = 0;
 
-                        if let Err(_err) = pixels.render() {
-                            window_target.exit();
+                        if ray_dir_x < 0.0 {
+                            step_x = -1;
+                            side_dist_x = (pos_x - map_x as f32) * delta_dist_x;
+                        } else {
+                            step_x = 1;
+                            side_dist_x = (map_x as f32 + 1.0 - pos_x) * delta_dist_x;
                         }
+
+                        if ray_dir_y < 0.0 {
+                            step_y = -1;
+                            side_dist_y = (pos_y - map_y as f32) * delta_dist_y;
+                        } else {
+                            step_y = 1;
+                            side_dist_y = (map_y as f32 + 1.0 - pos_y) * delta_dist_y;
+                        }
+
+                        while hit == 0 {
+                            if side_dist_x < side_dist_y {
+                                side_dist_x += delta_dist_x;
+                                map_x += step_x;
+                                side = 0;
+                            } else {
+                                side_dist_y += delta_dist_y;
+                                map_y += step_y;
+                                side = 1;
+                            }
+
+                            if MAP[map_x as usize][map_y as usize] > 0 {
+                                hit = 1;
+                            }
+                        }
+
+                        if side == 0 {
+                            perp_wall_dist = side_dist_x - delta_dist_x;
+                        } else {
+                            perp_wall_dist = side_dist_y - delta_dist_y;
+                        }
+
+                        let line_height = (SCREEN_HEIGHT as f32 / perp_wall_dist) as i32;
+
+                        let mut draw_start = -line_height / 2 + SCREEN_HEIGHT as i32 / 2;
+                        if draw_start < 0 {
+                            draw_start = 0;
+                        }
+                        let mut draw_end = line_height / 2 + SCREEN_HEIGHT as i32 / 2;
+                        if draw_end >= SCREEN_HEIGHT as i32 {
+                            draw_end = SCREEN_HEIGHT as i32 - 1;
+                        }
+
+                        let mut rgba = [0x5e, 0x48, 0xe8, 0xff];
+
+                        if side == 1 {
+                            rgba = [rgba[0] / 2, rgba[1] / 2, rgba[2] / 2, rgba[3]];
+                        }
+
+                        draw_vertical_line(
+                            x_stripe as usize,
+                            draw_start as usize,
+                            draw_end as usize,
+                            rgba,
+                            pixels.frame_mut(),
+                            SCREEN_WIDTH as usize,
+                        );
+                    }
+
+                    if let Err(_err) = pixels.render() {
+                        window_target.exit();
                     }
                 }
                 WindowEvent::Resized(size) => {
@@ -122,4 +230,18 @@ fn main() {
             _ => (),
         })
         .unwrap();
+}
+
+fn draw_vertical_line(
+    x: usize,
+    y_start: usize,
+    y_end: usize,
+    color: [u8; 4],
+    frame: &mut [u8],
+    width: usize,
+) {
+    for y in y_start..=y_end {
+        let idx = (y * width + x) * 4;
+        frame[idx..idx + 4].copy_from_slice(&color);
+    }
 }
