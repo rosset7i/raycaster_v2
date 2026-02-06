78d8a049f153cda7a4c8e1aaa2cf79ee447caf5d
Matheus Rossetti <mh.rossetti2002@gmail.com>
2026-02-05 21:51:03 -0300

    migracao modulo

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
+use crate::raycaster::Texture;
 use pixels::{Pixels, SurfaceTexture};
 use raycaster::Raycaster;
 use std::collections::{HashMap, HashSet};
 use std::error::Error;
-use std::time::{Duration, Instant};
 use winit::dpi::LogicalSize;
 use winit::event::{ElementState, Event, WindowEvent};
 use winit::window::WindowAttributes;
@@ -13,36 +13,11 @@ use winit::{
 
 mod raycaster;
 
-const SCREEN_WIDTH: u32 = 640;
-const SCREEN_HEIGHT: u32 = 480;
-
-enum Texture {
-    Color([u8; 4], [u8; 4]),
-}
-
-impl Texture {
-    fn color(&self, side: bool) -> &[u8; 4] {
-        match self {
-            Texture::Color(light, dark) => {
-                if side {
-                    dark
-                } else {
-                    light
-                }
-            }
-        }
-    }
-}
+const SCREEN_WIDTH: u32 = 1920;
+const SCREEN_HEIGHT: u32 = 1024;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    env_logger::builder()
-        .filter_level(log::LevelFilter::Info)
-        .init();
-
     let mut pressed_keys: HashSet<KeyCode> = HashSet::new();
-
-    let fallback_texture = Texture::Color([255, 0, 255, 255], [128, 0, 128, 255]); // debug pink
-
     let textures: HashMap<u8, Texture> = HashMap::from([
         (1, Texture::Color([94, 72, 232, 255], [47, 36, 116, 255])), // BLUE
         (2, Texture::Color([232, 72, 94, 255], [116, 36, 47, 255])), // RED
@@ -50,7 +25,6 @@ fn main() -> Result<(), Box<dyn Error>> {
     ]);
 
     let event_loop = EventLoop::new()?;
-
     let window = {
         let size = LogicalSize::new(SCREEN_WIDTH, SCREEN_HEIGHT);
 
@@ -78,11 +52,10 @@ fn main() -> Result<(), Box<dyn Error>> {
         move_speed: 0.05,
         rot_speed: 0.05,
         map: vec![],
+        screen_width: SCREEN_WIDTH,
+        screen_height: SCREEN_HEIGHT,
     };
     raycaster.load_map_from_file(None)?;
-
-    let mut now = Instant::now();
-    let mut frame_count: u32 = 0;
 
     #[allow(deprecated)]
     let res = event_loop.run(|event, window_target| match event {
@@ -110,90 +83,10 @@ fn main() -> Result<(), Box<dyn Error>> {
                     }
                 }
 
-                pixels.frame_mut().fill(0);
-                for vertical_stripe in 0..SCREEN_WIDTH {
-                    let camera_x = 2.0 * vertical_stripe as f32 / SCREEN_WIDTH as f32 - 1.0;
-                    let ray_dir_x = raycaster.dir_x + raycaster.plane_x * camera_x;
-                    let ray_dir_y = raycaster.dir_y + raycaster.plane_y * camera_x;
-
-                    let delta_dist_x = (1.0 / ray_dir_x).abs();
-                    let delta_dist_y = (1.0 / ray_dir_y).abs();
-
-                    let (mut side_dist_x, step_x) = if ray_dir_x < 0.0 {
-                        (raycaster.pos_x.fract() * delta_dist_x, -1)
-                    } else {
-                        ((1.0 - raycaster.pos_x.fract()) * delta_dist_x, 1)
-                    };
-
-                    let (mut side_dist_y, step_y) = if ray_dir_y < 0.0 {
-                        (raycaster.pos_y.fract() * delta_dist_y, -1)
-                    } else {
-                        ((1.0 - raycaster.pos_y.fract()) * delta_dist_y, 1)
-                    };
-
-                    let mut side: bool;
-                    let mut map_x = raycaster.pos_x as i32;
-                    let mut map_y = raycaster.pos_y as i32;
-                    let texture: &Texture;
-                    loop {
-                        if side_dist_x < side_dist_y {
-                            side_dist_x += delta_dist_x;
-                            map_x += step_x;
-                            side = false;
-                        } else {
-                            side_dist_y += delta_dist_y;
-                            map_y += step_y;
-                            side = true;
-                        }
-
-                        let cell = raycaster.map[map_x as usize][map_y as usize];
-
-                        if cell > 0 {
-                            texture = textures.get(&cell).unwrap_or(&fallback_texture);
-                            break;
-                        }
-                    }
-
-                    let perp_wall_dist = if !side {
-                        side_dist_x - delta_dist_x
-                    } else {
-                        side_dist_y - delta_dist_y
-                    };
-
-                    let line_height = (SCREEN_HEIGHT as f32 / perp_wall_dist) as i32;
-
-                    let mut draw_start = -line_height / 2 + SCREEN_HEIGHT as i32 / 2;
-                    if draw_start < 0 {
-                        draw_start = 0;
-                    }
-
-                    let mut draw_end = line_height / 2 + SCREEN_HEIGHT as i32 / 2;
-                    if draw_end >= SCREEN_HEIGHT as i32 {
-                        draw_end = SCREEN_HEIGHT as i32 - 1;
-                    }
-
-                    draw_vertical_line(
-                        vertical_stripe as usize,
-                        draw_start as usize,
-                        draw_end as usize,
-                        *texture.color(side),
-                        pixels.frame_mut(),
-                        SCREEN_WIDTH as usize,
-                    );
-                }
+                raycaster.draw_frame(pixels.frame_mut(), &textures);
 
                 if pixels.render().is_err() {
                     window_target.exit();
-                }
-
-                frame_count += 1;
-                let elapsed = now.elapsed();
-                if elapsed >= Duration::from_secs(1) {
-                    let fps = frame_count as f64 / elapsed.as_secs_f64();
-                    log::info!("fps: {:.1}", fps);
-
-                    frame_count = 0;
-                    now = Instant::now();
                 }
             }
             WindowEvent::Resized(size) => {
@@ -210,18 +103,4 @@ fn main() -> Result<(), Box<dyn Error>> {
     });
 
     Ok(res?)
-}
-
-fn draw_vertical_line(
-    line: usize,
-    y_start: usize,
-    y_end: usize,
-    color: [u8; 4],
-    frame: &mut [u8],
-    width: usize,
-) {
-    for y in y_start..=y_end {
-        let idx = (y * width + line) * 4;
-        frame[idx..idx + 4].copy_from_slice(&color);
-    }
 }
--- src/raycaster.rs
+++ src/raycaster.rs
@@ -1,8 +1,29 @@
 use std::{
+    collections::HashMap,
     fs,
     io::{Error, ErrorKind},
     num::ParseIntError,
 };
+
+pub enum Texture {
+    Color([u8; 4], [u8; 4]),
+}
+
+impl Texture {
+    fn color(&self, side: bool) -> &[u8; 4] {
+        match self {
+            Texture::Color(light, dark) => {
+                if side {
+                    dark
+                } else {
+                    light
+                }
+            }
+        }
+    }
+}
+
+const FALLBACK_TEXTURE: Texture = Texture::Color([255, 0, 255, 255], [128, 0, 128, 255]); // debug pink
 
 #[derive(Default)]
 pub struct Raycaster {
@@ -19,11 +40,84 @@ pub struct Raycaster {
     pub rot_speed: f32,
 
     pub map: Vec<Vec<u8>>,
+
+    pub screen_width: u32,
+    pub screen_height: u32,
 }
 
 impl Raycaster {
-    pub fn load_map(&mut self, map: Vec<Vec<u8>>) {
-        self.map = map;
+    pub fn draw_frame(&mut self, frame: &mut [u8], textures: &HashMap<u8, Texture>) {
+        frame.fill(0);
+        for vertical_stripe in 0..self.screen_width {
+            let camera_x = 2.0 * vertical_stripe as f32 / self.screen_width as f32 - 1.0;
+            let ray_dir_x = self.dir_x + self.plane_x * camera_x;
+            let ray_dir_y = self.dir_y + self.plane_y * camera_x;
+
+            let delta_dist_x = (1.0 / ray_dir_x).abs();
+            let delta_dist_y = (1.0 / ray_dir_y).abs();
+
+            let (mut side_dist_x, step_x) = if ray_dir_x < 0.0 {
+                (self.pos_x.fract() * delta_dist_x, -1)
+            } else {
+                ((1.0 - self.pos_x.fract()) * delta_dist_x, 1)
+            };
+
+            let (mut side_dist_y, step_y) = if ray_dir_y < 0.0 {
+                (self.pos_y.fract() * delta_dist_y, -1)
+            } else {
+                ((1.0 - self.pos_y.fract()) * delta_dist_y, 1)
+            };
+
+            let mut side: bool;
+            let mut map_x = self.pos_x as i32;
+            let mut map_y = self.pos_y as i32;
+            let texture: &Texture;
+            loop {
+                if side_dist_x < side_dist_y {
+                    side_dist_x += delta_dist_x;
+                    map_x += step_x;
+                    side = false;
+                } else {
+                    side_dist_y += delta_dist_y;
+                    map_y += step_y;
+                    side = true;
+                }
+
+                let cell = self.map[map_x as usize][map_y as usize];
+
+                if cell > 0 {
+                    texture = textures.get(&cell).unwrap_or(&FALLBACK_TEXTURE);
+                    break;
+                }
+            }
+
+            let perp_wall_dist = if !side {
+                side_dist_x - delta_dist_x
+            } else {
+                side_dist_y - delta_dist_y
+            };
+
+            let line_height = (self.screen_height as f32 / perp_wall_dist) as i32;
+
+            let mut draw_start = -line_height / 2 + self.screen_height as i32 / 2;
+            if draw_start < 0 {
+                draw_start = 0;
+            }
+
+            let mut draw_end = line_height / 2 + self.screen_height as i32 / 2;
+            if draw_end >= self.screen_height as i32 {
+                draw_end = self.screen_height as i32 - 1;
+            }
+
+            draw_vertical_line(
+                vertical_stripe as usize,
+                draw_start as usize,
+                draw_end as usize,
+                *texture.color(side),
+                frame,
+                self.screen_width as usize,
+            );
+        }
     }
 
     pub fn load_map_from_file(&mut self, path: Option<&str>) -> Result<(), Error> {
@@ -90,5 +184,19 @@ impl Raycaster {
         let old_plane_x = self.plane_x;
         self.plane_x = self.plane_x * cos - self.plane_y * sin;
         self.plane_y = old_plane_x * sin + self.plane_y * cos;
+    }
+}
+
+fn draw_vertical_line(
+    line: usize,
+    y_start: usize,
+    y_end: usize,
+    color: [u8; 4],
+    frame: &mut [u8],
+    width: usize,
+) {
+    for y in y_start..=y_end {
+        let idx = (y * width + line) * 4;
+        frame[idx..idx + 4].copy_from_slice(&color);
     }
 }
